a356c765a18433b6bf9b84bb42044b118dd7ddce
Rohan Mahajan <rohanm789@gmail.com>
2023-03-16 05:00:02 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Demo from "./Demo";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <Demo />
+  </div>,
+  rootElement
+);
